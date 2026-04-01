1c102486965dffa30bf8cf40ee2c0f4cb12886de
enterprise-security-home-lab <raymond.bagnerise@gmail.com>
2026-04-01 16:16:02 -0700

    Phase 19 complete: packet capture and Wireshark analysis of vsftpd exploit

---
+++ Scripts/SetVMnetIPs.ps1
@@ -0,0 +1,3 @@
+﻿netsh interface ip set address name="VMware Network Adapter VMnet19" static 10.10.30.2 255.255.255.0
+netsh interface ip set address name="VMware Network Adapter VMnet10" static 10.10.10.2 255.255.255.0
+Set-NetIPInterface -InterfaceAlias "VMware Network Adapter VMnet19" -AddressFamily IPv4 -Forwarding Enabled
